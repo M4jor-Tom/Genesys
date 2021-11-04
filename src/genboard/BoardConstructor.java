116fdef1d6c04d30f159d1b27f1013d1dcb85f03
Tom <tom-vautray@hotmail.fr>
2021-11-04 17:54:30 +0100

    Filled class BoardConstructor

--- src/genboard/BoardConstructor.java
+++ src/genboard/BoardConstructor.java
@@ -2,35 +2,96 @@ package genboard;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Random;
 
 import boards.Board;
+import boards.Ratio;
 import generic.AbstractPopulationConstructor;
 
 public class BoardConstructor extends AbstractPopulationConstructor<Board>{
 
+	private static int populatioSize = 10;
+	private static Random random = new Random();
+	
 	@Override
 	public Board crossover(Board populator1, Board populator2) {
-		// TODO Auto-generated method stub
-		return null;
+		Ratio meanWheelHardness = new Ratio(
+			(
+				populator1.getWheelHardness().getValue()
+				+ populator2.getWheelHardness().getValue()
+			) / 2,
+			true
+		);
+		Ratio meanTurning = new Ratio(
+			(
+				populator1.getTurning().getValue()
+				+ populator2.getTurning().getValue()
+			) / 2,
+			true
+		);
+		
+		return new Board(meanWheelHardness, meanTurning);
 	}
 
 	@Override
 	protected Board naiveMutation(Board populator) {
-		// TODO Auto-generated method stub
-		return null;
+		Ratio randomizedRatio = new Ratio(random.nextDouble(), true);
+		
+		if(random.nextBoolean())
+		{
+			populator.setWheelHardness(randomizedRatio);
+		}
+		else
+		{
+			populator.setTurning(randomizedRatio);
+		}
+			
+		return populator;
 	}
 
 	@Override
 	protected Board closeMutation(Board populator) {
-		// TODO Auto-generated method stub
-		return null;
+		Ratio ratioToRandomize = null;
+		boolean hardnessNotTurning = random.nextBoolean();
+		
+		if(hardnessNotTurning) {
+			ratioToRandomize = populator.getWheelHardness();
+		}
+		else {
+			ratioToRandomize = populator.getTurning();
+		}
+		
+		Double sign = random.nextBoolean()
+			? 1.0
+			: -1.0;
+		
+		ratioToRandomize.setValue(
+				ratioToRandomize.getValue()
+				+ sign * random.nextDouble() * 0.5
+		);
+		
+		if(hardnessNotTurning) {
+			populator.setWheelHardness(ratioToRandomize);
+		}
+		else {
+			populator.setTurning(ratioToRandomize);
+		}
+		
+		return populator;
 	}
 
 	@Override
 	public Collection<Board> initalizePopulation() {
 		ArrayList<Board> boardArrayList = new ArrayList<Board>();
 		
-		
+		for(int i = 0; i < populatioSize; i++) {
+			boardArrayList.add(
+				new Board(
+					new Ratio(random.nextDouble(), true),
+					new Ratio(random.nextDouble(), true)
+				)
+			);
+		}
 		
 		return boardArrayList;
 	}
