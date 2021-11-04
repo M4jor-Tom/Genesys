ec80d4c5139cf3292d28fe5f279f7d56ecca1b98
Tom <tom-vautray@hotmail.fr>
2021-11-04 17:54:13 +0100

    Added strict option at Ratio construction, and made setters public

--- src/boards/Ratio.java
+++ src/boards/Ratio.java
@@ -3,21 +3,54 @@ package boards;
 public class Ratio {
 	private double value;
 
-	public Ratio(double value) {
+	public Ratio(double value, boolean strict) {
 		super();
 		
-		setValue(value);
+		if(strict) {
+			strictSetValue(value);
+		}
+		else {
+			setValue(value);
+		}
 	}
 
 	public double getValue() {
 		return value;
 	}
+	
+	public void setValue(double value)
+	{
+		if(Ratio.valueIsTooHigh(value)) {
+			this.value = 1.0;
+		}
+		else if(Ratio.valueIsTooLow(value)) {
+			this.value = 0.0;
+		}
+		else {
+			this.value = value;
+		}
+	}
 
-	private void setValue(double value) {
-		if(value > 1.0 || value < 0.0) {
+	public void strictSetValue(double value) {
+		this.value = Double.NaN;
+		
+		if(Ratio.valueIsOutOfBounds(value)) {
 			System.out.println("Ratio: value (" + value + ") out of bounds (0.0 - 1.0)");
 		}
-		
-		this.value = value;
+		else {
+			this.value = value;
+		}
+	}
+	
+	private static boolean valueIsTooHigh(double value) {
+		return value > 1.0;
+	}
+	
+	private static boolean valueIsTooLow(double value) {
+		return value < 0.0;
+	}
+	
+	private static boolean valueIsOutOfBounds(double value) {
+		return Ratio.valueIsTooHigh(value) || Ratio.valueIsTooLow(value);
 	}
 }
