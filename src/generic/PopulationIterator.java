cd2c9f9fef677a258b341381ec4461225fed06fd
Tom <tom-vautray@hotmail.fr>
2021-11-04 19:22:33 +0100

    Added PopulationIterator.iterate() and required methods
    -pickPopulator()
    -removeLowestFittedPopulator()

--- src/generic/PopulationIterator.java
+++ src/generic/PopulationIterator.java
@@ -35,9 +35,91 @@ public class PopulationIterator<Populator> {
 		setMutationProbability(mutationProbability);
 	}
 	
-	public void iterate() {
-	}
+	private Populator pickPopulator(Populator otherThanPopulator) {
+		Double totalFitness = 0.0;
 		
+		//Summing all fitnesses
+		for(Populator populator: getPopulation()) {
+			totalFitness += getEnvironment().getFitness(populator);
+		}
+		
+		//Selecting a fitness cursor smaller than the sum of all of them
+		Double fitnessCursor = totalFitness * random.nextDouble();
+		
+		//Selecting a fitness wheighted random populator
+		for(Populator populator: getPopulation()) {
+			fitnessCursor -= getEnvironment().getFitness(populator);
+			
+			if(fitnessCursor < 0.0) {
+				if(populator.equals(otherThanPopulator)) {
+					//Picked preselected parent, have to find another instead so searches again
+					return pickPopulator(otherThanPopulator);
+				}
+				else {
+					return populator;
+				}
+			}
+		}
+		
+		(new Exception("Error finding a parent")).printStackTrace();
+		
+		return null;
+	}
+	
+	private void removeLowestFittedPopulator() {
+		Populator lowestFitnessPopulator = null;
+		
+		for(Populator populator: getPopulation()) {
+			
+			if(lowestFitnessPopulator == null) {
+				//Selecting first populator
+				lowestFitnessPopulator = populator;
+			}
+			else {
+				//Selecting lowest fitted populator
+				if(
+					getEnvironment().getFitness(lowestFitnessPopulator)
+					> getEnvironment().getFitness(populator)
+				) {
+					//Lowest fitness populator changes to the current one
+					lowestFitnessPopulator = populator;
+				}
+			}
+		}
+		
+		//Removing lowest fitness populator from population
+		getPopulation().remove(lowestFitnessPopulator);
+	}
+	
+	public void iterate() {
+		//Picking different parents
+		Populator parent1 = pickPopulator(null);
+		Populator parent2 = pickPopulator(parent1);
+		
+		//Borning a new populator from two others
+		Populator populator = getPopulationConstructor().crossover(parent1, parent2);
+
+		//Replacing the lowest fitted populator in list
+		addPopulator(populator);
+		removeLowestFittedPopulator();
+		
+		//Randomly mutating populators
+		for(Populator iterationPopulator: getPopulation()) {
+			getPopulationConstructor().mutation(iterationPopulator, getMutationProbability());
+		}
+	}
+	
+	public String populationToString()
+	{
+		String ret = "";
+		
+		for(Populator populator: getPopulation()) {
+			ret += populator.toString() + "\n";
+		}
+		
+		return ret;
+	}
+	
 	public void addPopulator(Populator populator) {
 		List<Populator> population = getPopulation();
 		population.add(populator);
