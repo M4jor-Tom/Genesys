dc2c0c6a26721828f904d9aae40b3c351991a2e4
Tom <tom-vautray@hotmail.fr>
2021-11-04 19:42:47 +0100

    Added setup console interface in MainClass.main()

--- src/mainpackage/MainClass.java
+++ src/mainpackage/MainClass.java
@@ -1,18 +1,40 @@
 package mainpackage;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import boards.Board;
 import genboard.BoardConstructor;
+import genboard.SkateParkEnvironment;
 import genboard.SlickRoadEnvironment;
+import generic.AbstractEnvironment;
 import generic.PopulationIterator;
 import generic.Ratio;
 
 public class MainClass {
+	private static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		
+		System.out.println("Population size: ");
+		int populationSize = scanner.nextInt();
+		
+		System.out.println("Environment:\n1.SkatePark\n2(or any).Slick road");
+		int environmentChoice = 0;
+		try{
+			environmentChoice = scanner.nextInt();
+		}
+		catch(InputMismatchException e) {
+			
+		}
+		
+		AbstractEnvironment<Board> environment = environmentChoice == 1
+			? new SkateParkEnvironment()
+			: new SlickRoadEnvironment();
 
 		PopulationIterator<Board> populationIterator = new PopulationIterator<Board>(
-					new BoardConstructor(10),
-					new SlickRoadEnvironment(),
+					new BoardConstructor(populationSize),
+					environment,
 					new Ratio(0.1, true)
 		);
 		
